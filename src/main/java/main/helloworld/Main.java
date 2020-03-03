861b66719324066ba3c0a3b40e5cfa5cdfcbd548
Dragan Jovanović <draganjoov@gmail.com>
2020-03-03 01:47:24 +0100

    Initial commit

---
+++ src/main/java/main/helloworld/Main.java
@@ -0,0 +1,19 @@
+package main.helloworld;
+
+/**
+ *
+ * @author Dragan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       System.out.println("Hello World!");
+       System.out.println("Dragan");
+       
+    }
+    
+}
